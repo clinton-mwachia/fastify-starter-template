e38c9b88410ab03bea6fa7c31ef23361cf734e38
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-10-16 16:03:08 +0300

    implemented logger middleware and added add test route

---
+++ index.js
@@ -0,0 +1,52 @@
+// entry file
+const fastify = require("fastify")({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+    },
+  },
+});
+const mongoose = require("mongoose");
+const LoggerMiddleware = require("./helpers/logger");
+require("dotenv").config();
+
+/** start middlewares */
+fastify.addHook("preHandler", LoggerMiddleware);
+/** end middlewares */
+
+/** start routes */
+fastify.get("/", async (request, reply) => {
+  reply.send("hello");
+});
+/** end routes */
+
+/** start constants */
+const DB = process.env.DB;
+const PORT = process.env.PORT;
+/** end constants */
+
+/**
+ * connect to the db
+ */
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    fastify.log.info("DB SUCCESSFULLY CONNECTED!!!");
+  })
+  .catch((err) => fastify.log.error(err.message));
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    fastify.listen({ port: PORT, host: "0.0.0.0" });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
