e0b0bdc6596679df22be2c4a6b83d05222ba141a
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-10-18 13:15:49 +0300

    added function to register a user

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,23 @@
  * user routes
  */
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 async function UserRoutes(fastify) {
-  /** start get users */
-  fastify.get("/", async (request, reply) => {
-    reply.send("users");
+  /** start register users */
+  fastify.post("/user/register", async (request, reply) => {
+    try {
+      const user = new User(request.body);
+      user.password = bcrypt.hashSync(request.body.password, 10);
+
+      await user.save();
+
+      reply.send({ message: "User Account Created!" });
+    } catch (error) {
+      reply.status(500).send({ message: "Error creating user account", error });
+    }
   });
-  /** start get users */
+  /** start register users */
 }
 
 module.exports = UserRoutes;
