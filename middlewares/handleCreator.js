c9b1bdc94ad19242c125e7f2880a649fa34a1d5e
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-10-23 16:36:52 +0300

    deleted middleware folder

--- middlewares/handleCreator.js
+++
@@ -1,9 +0,0 @@
-//const mongoose = require("mongoose");
-//const User = require("../models/user");
-
-// a middleware to check if the user is getting the todo they created
-const isCreatorSingleTodo = async (request, reply) => {
-  // TODO
-};
-
-module.exports = isCreatorSingleTodo;
