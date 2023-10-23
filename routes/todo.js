c836e47c5c893fc3ce645ebf55339e1fc0a66899
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-10-23 11:33:54 +0300

    added function to register todo

--- routes/todo.js
+++ routes/todo.js
@@ -1,10 +1,28 @@
 /** todos routers
  *
  */
+const Todo = require("../models/todo");
+const User = require("../models/user");
+
 async function TodoRoutes(fastify) {
-  fastify.get("/todo", async (request, reply) => {
-    reply.send({ message: "test" });
+  /** start insert a single todo */
+  fastify.post("/todo/register", async (request, reply) => {
+    try {
+      const user = await User.findById(request.body.user);
+
+      if (!user) {
+        reply.status(404).send({ message: "User not found" });
+      } else {
+        const todo = new Todo(request.body);
+        await todo.save();
+
+        reply.send({ message: "User Account Created" });
+      }
+    } catch (err) {
+      reply.status(500).send({ message: "Error inserting todo", err });
+    }
   });
+  /** end insert a single todo */
 }
 
 module.exports = TodoRoutes;
